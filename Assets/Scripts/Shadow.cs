991f9bed4f3b3106cc6a497af92f7f18c49a4266
Ayla Myers <bridgs.dev@gmail.com>
2021-11-02 20:43:00 -0400

    Tweaking the perspective a bit

--- Assets/Scripts/Shadow.cs
+++ Assets/Scripts/Shadow.cs
@@ -2,9 +2,6 @@ using UnityEngine;
 
 namespace StrikeOut {
 	public class Shadow : MonoBehaviour {
-		[Header("Children")]
-		[SerializeField] private Transform visuals;
-
 		private void LateUpdate () {
 			transform.position = new Vector3(
 				transform.position.x,

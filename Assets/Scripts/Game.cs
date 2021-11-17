e30eb2aa894f170eebd3883b4fd62953e7acedeb
Ayla Myers <bridgs.dev@gmail.com>
2021-11-17 15:47:26 -0500

    Disable domain reloading for faster play mode

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,6 +3,9 @@ using SharedUnityMischief;
 
 namespace StrikeOut {
 	public class Game : SingletonMonoBehaviour<Game> {
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+		protected static void Reset () => ResetInstance();
+
 		[Header("Game Config")]
 		[SerializeField] private bool _debugMode = true;
 
